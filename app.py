80980205872becde439e60f0de22f94711ec63fe
dhruvawasthi22 <55012860+dhruvawasthi22@users.noreply.github.com>
2026-06-01 01:21:34 +0530

    Refactor Streamlit app layout and styles
    Updated CSS styles to hide Streamlit header and adjust padding.

--- app.py
+++ app.py
@@ -6,24 +6,16 @@ st.set_page_config(page_title="Dremel Trend Dashboard", layout="wide")
 
 # CSS to hide the default Streamlit borders and menus so Looker takes over the whole screen
 st.markdown("""
-        <style>
-               .css-18e3th9 {
-                    padding-top: 0rem;
-                    padding-bottom: 10rem;
-                    padding-left: 5rem;
-                    padding-right: 5rem;
-                }
-               .css-1d391kg {
-                    padding-top: 3.5rem;
-                    padding-right: 1rem;
-                    padding-bottom: 3.5rem;
-                    padding-left: 1rem;
-                }
-        </style>
-        """, unsafe_allow_html=True)
-
-
-
+    <style>
+    [data-testid="stHeader"] {display: none !important;}
+    
+    /* Using the exact VIP selector you found online! */
+    div.block-container {
+        padding-top: 0rem !important;
+        margin-top: -2rem !important;
+    }
+    </style>
+""", unsafe_allow_html=True)
 # --- PASTE YOUR LOOKER STUDIO EMBED URL BELOW ---
 looker_url = "https://datastudio.google.com/embed/reporting/81450734-233d-4c12-b61c-17770e06360b/page/VxqzF"
 
